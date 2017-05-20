60a39a696a57ae61e7cb591999ce3b57739eb27f
SamDriesen <sam.driesen@student.kdg.be>
2017-05-20 14:34:19 +0200

    Score

--- DigitalBroadcastOpdracht/src/hellotvxlet/HelloTVXlet.java
+++ DigitalBroadcastOpdracht/src/hellotvxlet/HelloTVXlet.java
@@ -6,32 +6,36 @@ import javax.tv.xlet.*;
 import org.havi.ui.HScene;
 import org.havi.ui.HSceneFactory;
 import org.havi.ui.HStaticText;
-import org.havi.ui.HTextButton;
+//import org.havi.ui.HTextButton;
 import org.havi.ui.HVisible;
 import org.havi.ui.event.HActionListener;
 
 
 public class HelloTVXlet implements Xlet, HActionListener{
 public HScene scene;
+public HStaticText label1;
+MijnComponent mc1;
+
   
     public HelloTVXlet() {
         
     }
-
+    
     public void initXlet(XletContext ctx) throws XletStateChangeException {
-       
+      mc1 = new MijnComponent(0,0,720,576);
       scene=HSceneFactory.getInstance().getDefaultHScene();
-      
-      HStaticText label1=new HStaticText("Wie was de eerste president van de VS?",30,30,600,100);//x,y,b,h
+      label1= new HStaticText("Score : " + mc1.getScore(), 5 , 30 , 125, 30);
       label1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      label1.setBackground(Color.GRAY);
-        
-      MijnComponent mc1=new MijnComponent(0,0,720,576);
-      scene.add(mc1);
+      label1.setBackground(Color.BLACK);      
+
+      
       scene.add(label1);
+      scene.add(mc1);
         
         scene.validate(); scene.setVisible(true);
     }
+    
+   
 
     public void startXlet() {
     
